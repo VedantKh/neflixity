654be3ca2d77e9bcef614a47a3fb17ee3d784781
Vedant Khanna <vedantkhanna@gmail.com>
2025-02-13 02:50:09 -0800

    configure server for railway deployment

--- server/server.py
+++ server/server.py
@@ -249,5 +249,6 @@ def vector_search():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
